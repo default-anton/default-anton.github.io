2130cabf8635a7d950357a19f76922f755248a3d
Anton Kuzmenko <1917237+default-anton@users.noreply.github.com>
2024-09-02 23:03:02 -0700

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_includes/**/*.{html,md}',
+    './_layouts/**/*.{html,md}',
+    './_pages/**/*.{html,md}',
+    './_posts/**/*.{html,md}',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [],
+}
